fc63531e3b1569654a11116d50bcb2760eeec21d
eltonn <elton.santana@gmail.com>
2021-02-25 14:50:27 -0300

    add example env file and add variablesadd example env file and add variables the connect in servers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
-# bucardo install
+# install
 bucardo install --batch 
 
-# start bucardo
+# start
 bucardo start
 
 # add servidores
-bucardo add dbdatabase hetzner dbhost=172.24.0.2 dbport=25432 dbname=dengue dbuser=dengueadmin dbpass=aldengue
-bucardo add dbdatabase fgv dbhost=172.24.0.3 dbport=35432 dbname=dengue dbuser=dengueadmin dbpass=aldengue
+bucardo add dbdatabase hetzner dbhost=$HOST_HETZNER dbport=$PORT_HETZNER dbname=$DB_HETZNER dbuser=$USER_HETZNER dbpass=$PASS_HETZNER
+bucardo add dbdatabase fgv dbhost=$HOST_FGV dbport=$PORT_FGV dbname=$DB_FGV dbuser=$USER_FGV dbpass=$PASS_FGV
 
 # add banco de dados
 bucardo add table % db=hetzner
@@ -19,3 +19,6 @@ bucardo add all tables --herd=fgv_hetzner db=fgv
 # add sync
 bucardo add sync sync_hetznerTofgv relgroup=hetzner_fgv db=hetzner,fgv
 bucardo add sync sync_fgvTohetzner relgroup=fgv_hetzner db=fgv,hetzner
+
+# status
+bucardo status all
